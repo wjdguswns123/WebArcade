c7a9da10cc22e98af7fdb07ceb03b95b9dbfd33e
wjdguswns123 <archangel123@naver.com>
2025-02-25 21:58:13 +0900

    - 키 입력 ui 터치 이벤트 추가.

--- src/components/JoystickUI.tsx
+++ src/components/JoystickUI.tsx
@@ -15,16 +15,40 @@ interface JoystickUIProps {
 const JoystickUI:React.FC<JoystickUIProps> = ({onUpMoveStart, onUpMoveEnd, onDownMoveStart, onDownMoveEnd, onLeftKeyStart, onLeftKeyEnd, onRightKeyStart, onRightKeyEnd}) => {
   return (
     <div className="joystick">
-      <div className="arrow-button up-arrow" onMouseDown={onUpMoveStart} onMouseUp={onUpMoveEnd} onMouseLeave={onUpMoveEnd} >
+      <div className="arrow-button up-arrow" 
+        onMouseDown={onUpMoveStart} 
+        onMouseUp={onUpMoveEnd} 
+        onMouseLeave={onUpMoveEnd}
+        onTouchStart={onUpMoveStart}
+        onTouchEnd={onUpMoveEnd}
+      >
         <img className="arrow-img" src="Images/Icons/arrow.png" alt="" />
       </div>
-      <div className="arrow-button left-arrow" onMouseDown={onLeftKeyStart} onMouseUp={onLeftKeyEnd} onMouseLeave={onLeftKeyEnd} >
+      <div className="arrow-button left-arrow" 
+        onMouseDown={onLeftKeyStart} 
+        onMouseUp={onLeftKeyEnd} 
+        onMouseLeave={onLeftKeyEnd}
+        onTouchStart={onLeftKeyStart}
+        onTouchEnd={onLeftKeyEnd}
+      >
         <img className="arrow-img" src="Images/Icons/arrow.png" alt="" />
       </div>
-      <div className="arrow-button right-arrow" onMouseDown={onRightKeyStart} onMouseUp={onRightKeyEnd} onMouseLeave={onRightKeyEnd} >
+      <div className="arrow-button right-arrow" 
+        onMouseDown={onRightKeyStart} 
+        onMouseUp={onRightKeyEnd} 
+        onMouseLeave={onRightKeyEnd}
+        onTouchStart={onRightKeyStart}
+        onTouchEnd={onRightKeyEnd}
+      >
         <img className="arrow-img" src="Images/Icons/arrow.png" alt="" />
       </div>
-      <div className="arrow-button down-arrow" onMouseDown={onDownMoveStart} onMouseUp={onDownMoveEnd} onMouseLeave={onDownMoveEnd} >
+      <div className="arrow-button down-arrow" 
+        onMouseDown={onDownMoveStart}
+        onMouseUp={onDownMoveEnd}
+        onMouseLeave={onDownMoveEnd}
+        onTouchStart={onDownMoveStart}
+        onTouchEnd={onDownMoveEnd}
+      >
         <img className="arrow-img" src="Images/Icons/arrow.png" alt="" />
       </div>
     </div>
